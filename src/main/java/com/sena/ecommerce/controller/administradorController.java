411b5bd854275242cbf8d22766d2635099701181
jataba <javier_tabares@soy.sena.edu.co>
2024-10-18 13:04:34 -0500

    v0.3_vistaAdmin_vistaTemplateAdmin_html_administradorController_18_102024

---
+++ src/main/java/com/sena/ecommerce/controller/administradorController.java
@@ -0,0 +1,22 @@
+package com.sena.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/administrador")
+public class administradorController {
+	
+	
+	@GetMapping("")
+	public String home() {
+		return "administrador/home";
+		
+	}
+	
+	
+	
+	
+
+}
