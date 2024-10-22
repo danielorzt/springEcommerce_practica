9155bfe7c46b6f609613f06764ca995f354e202c
jataba <javier_tabares@soy.sena.edu.co>
2024-10-22 14:17:04 -0500

    v06_creacionDeClasesDeTipoImplementPaqueteService_22102024_jataba

---
+++ src/main/java/com/sena/ecommerce/service/UsuarioServiceImplement.java
@@ -0,0 +1,61 @@
+package com.sena.ecommerce.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sena.ecommerce.model.Usuario;
+import com.sena.ecommerce.repository.IUsuarioRepository;
+
+@Service
+public class UsuarioServiceImplement implements IUsuarioService {
+
+	// Esto es objeto de tipo privado que es de tipo repositorio (Instancia)
+	@Autowired
+	private IUsuarioRepository usuarioRpository;
+
+	@Override
+	public Usuario save(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return usuarioRpository.save(usuario);
+	}
+
+	@Override
+	public Optional<Usuario> get(Integer id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public void update(Usuario usuario) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Optional<Usuario> findById(Integer id) {
+		// TODO Auto-generated method stub
+		return usuarioRpository.findById(id);
+	}
+
+	@Override
+	public Optional<Usuario> findByEmail(String email) {
+		// TODO Auto-generated method stub
+		return usuarioRpository.finByEmail(email);
+	}
+
+	@Override
+	public List<Usuario> findAll() {
+		// TODO Auto-generated method stub
+		return usuarioRpository.findAll();
+	}
+
+}
